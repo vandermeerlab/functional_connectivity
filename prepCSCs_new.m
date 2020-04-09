051e665054e1cda1ca3a230658d37b0f6e18f831
stottbot <stott.jeffrey@gmail.com>
2020-04-09 17:17:57 -0400

    cfg structure changed

--- prepCSCs_new.m
+++ prepCSCs_new.m
@@ -18,36 +18,27 @@ tic
 LoadExpKeys;
 
 %% Load the CSCs
-cfg_ofc.fc = {ExpKeys.OFCcsc};
-cfg_ofc.VoltageConvFactor = cfg_master.VoltageConvFactor;
-cfg_ofc.decimateByFactor = cfg_master.decimateFactor;
-ofc = LoadCSC(cfg_ofc);
+cfg_master.fc = {ExpKeys.OFCcsc};
+ofc = LoadCSC(cfg_master);
 ofc_dt = median(diff(ofc.tvec));
 
-cfg_vstr.fc = {ExpKeys.VSTRcsc};
-cfg_vstr.VoltageConvFactor = cfg_master.VoltageConvFactor;
-cfg_vstr.decimateByFactor = cfg_master.decimateFactor;
-vstr = LoadCSC(cfg_vstr);
+cfg_master.fc = {ExpKeys.VSTRcsc};
+vstr = LoadCSC(cfg_master);
 vstr_dt = median(diff(vstr.tvec));
 
-assert(length(ofc.data)==length(vstr.data));
-assert(ofc_dt == vstr_dt);
-
 if strcmp('NaN', ExpKeys.HIPPcsc) == 0 && cfg_master.hippflag == 1;
-    cfg_hipp.fc = ExpKeys.HIPPcsc;
-    cfg_hipp.VoltageConvFactor = cfg_master.VoltageConvFactor;
-    cfg_hipp.decimateByFactor = cfg_master.decimateFactor;
-    [hipp] = LoadCSC(cfg_hipp);
+    cfg_master.fc = ExpKeys.HIPPcsc;
+    [hipp] = LoadCSC(cfg_master);
     hipp_dt = median(diff(hipp.tvec));
 end
 
 %% restrict to time on track
-if cfg_def.doRestrict == 1;
+if cfg_master.doRestrict == 1;
     ofc = restrict(ofc, ExpKeys.TimeOnTrack, ExpKeys.TimeOffTrack);
     vstr = restrict(vstr, ExpKeys.TimeOnTrack, ExpKeys.TimeOffTrack);
 end
 
-if cfg_def.doRestrict == 1 && cfg_def.hippflag == 1;
+if cfg_master.doRestrict == 1 && cfg_def.hippflag == 1;
     hipp = restrict(hipp, ExpKeys.TimeOnTrack, ExpKeys.TimeOffTrack);
 end
 
