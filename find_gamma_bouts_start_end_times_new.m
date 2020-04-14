96854da7de767b4b9c89a3fedcc9750371096d11
stottbot <stott.jeffrey@gmail.com>
2020-04-14 12:17:31 -0400

    working version of gamma event times detection

--- find_gamma_bouts_start_end_times_new.m
+++ find_gamma_bouts_start_end_times_new.m
@@ -1,73 +1,83 @@
 function [gammaIndicesStart, gammaIndicesEnd, gammaTimesStart, gammaTimesEnd, X] = find_gamma_bouts_start_end_times_new(cfg_in)
+% 2020-04-14. JJS. This function gets the start and end times of threshold crossing events for the filtered gamma LFP. 
+
 % Inputs:
-%   lowpass:        lowpass filter value (45 for g50, 70 for g80)
-%   highpass:       highpass filter value (65 for g50, 90 for g80)
-%   boutDuration:   how long you want the gamma bouts to be (in s). This parameter is chosen to make them all the same length, but in reality, gamma bouts
-%                       will vary in duration.
+%   lowpass:            lowpass filter value (45 for g50, 70 for g80)
+%   highpass:           highpass filter value (65 for g50, 90 for g80)
+%   boutDuration:       how long you want the gamma bouts to be (in s). This parameter is chosen to make them all the same length, but in reality, gamma bouts
+%                           will vary in duration.
 % Outputs:
-%   amplitude_peaks = timestamps of gamma amplitude peaks
-%
+%   gammaIndicesStart:  tvec indices of the start of gamma events 
+%   gammaIndicesEnd:    tvec indices of the end of gamma events
+%   gammaTimesStart:    timestamps of the start of gamma events
+%   gammaTimesEnd:      timestamps of the end of gamma events
+%   X:                  nChannels x Samples x nGammaEvents           
+                            % Channel 1 = OFC. Channel 2 = vstr     % can add Hipp later as Channel 3
+                            % nSamples = number of data points in a gamma event (proportional to boutDuration)
+                            % nGammaEvents = number of gamma events in the session 
+
 fd = FindFiles('*keys.m');
 startSess = 1; endSess = length(fd);
 
+cfg_def = [];
+cfg_def.doPlot = 1;
 cfg_def.MarkerSize = 20;
-cfg_def.SaveIt = 1;
-cfg_def.doPlot = 0;
-cfg_def.decimateFactor = 2;
-cfg_def.doRestrict = 1;
-cfg_def.FiltOrder = 256;
-cfg_def.lowpass = 45;
-cfg_def.highpass = 65;
-cfg_def.boutDuration = 0.1;
-master_cfg = ProcessConfig(cfg_def,cfg_in);
+cfg_def.SaveIt = 0;
+cfg_def.boutDuration = 0.1;  % Duration (in seconds) of the gamma events. This is somewhat arbitrary. Julien used 0.4 s, Eric used 0.1 s 
+cfg_out = ProcessConfig(cfg_def,cfg_in);
 
 for iSess = startSess:endSess;
     pushdir(fileparts(fd{iSess}));
     SSN = GetSSN('SingleSession'); disp(SSN);
     
-    [ofc, vstr, ~, ~] = prepCSCs_new(master_cfg);   % can change this later to return hippocampus LFP also 
+    cfg_prep = [];
+    cfg_prep.decimateByFactor = 2; % Downsampling the data. Default here is 2 (from 2kHz to 1kHz).
+    cfg_prep.detrend = 1;   % for removing slow DC shifts in voltage
+    cfg_prep.diffdata = 0;  % for removing autocorrelation in the time series
+    cfg_prep.doRestrict = 1; % Restirct CSC to track time only.
+    cfg_prep.hippflag = 0;  % Do we want to Load a hippocampal CSC? (for sessions with electrode in fissure). 0 = no. 1 = yes.
+    [ofc, vstr, ~, ~] = prepCSCs_new(cfg_prep);   % can change this later to return hippocampus LFP also
+    fs = 1/median(diff(vstr.tvec));  % sampling rate of the LFP
     
-    %% NEW
-    % filter in low gamma band
-    cfg = [];
-    cfg.f = [master_cfg.lowpass master_cfg.highpass];
-    cfg.display_filter = 0;   
-    fvstr = FilterLFP(cfg,vstr);
+    %% Filter in low gamma band
+    cfg_filter = [];
+    cfg_filter.f = [45 65];  % frequency band to filter in. So far, just using low gamma events [45 65]
+    cfg_filter.display_filter = 0;
+    f_vstr = FilterLFP(cfg_filter,vstr);
     
-        %%  OLD
-%     disp('calculating power')
-%     % [IF, IA, IP, CSC0, IE]=InstSig(vstr,45,65,256);
-%     [~, ~, ~, CSC0, IE] = InstSig(vstr,lowpass,highpass,FiltOrder);
-%     percentile = .95;
-%     IEsort = sort(IE.data);
-%     percentile_index = percentile*length(IEsort);
-%     Amplitudecutoff = IEsort(round(percentile_index));
-%     TimeStepsToChopOFF = 4*round(boutDuration*fs);
-%     numBoutSamples = round(boutDuration*fs/2);  % number of samples on either side of the peak of a gamma event
-%     if mod(numBoutSamples,2) == 1;
-%         numBoutSamples = numBoutSamples-1;
-%     end  
-    %%
-    disp('finding threshold crossings')
+    %% Calculate gamma power
+    cfg_power = [];
+    IE = LFPpower(cfg_power,f_vstr);    % p_vstr is a TSD of power versus time of the filtered LFP trace.
+    percentile = .95;                   % Find peaks in the amplitude envelope that are above the 95% level. 
+    IEsort = sort(IE.data);
+    percentile_index = percentile*length(IEsort);
+    Amplitudecutoff = IEsort(round(percentile_index));
+    TimeStepsToChopOFF = 2*round(cfg_out.boutDuration*fs); % How many samples to ignore at the end of the session...so that the last gamma event doesn't exceed the last timestep
+    numBoutSamples = round(cfg_out.boutDuration*fs/2);  % number of samples on either side of the peak of a gamma event to include 
+    if mod(numBoutSamples,2) == 1;
+        numBoutSamples = numBoutSamples-1;
+    end
+    
+    %% Find Threshold Crossings 
     % Get timestamps for the first instance of each threshold crossing for the gamma amplitude envelope
-    [amplitdue_index,~] = find(IE.data>=Amplitudecutoff);
-    event_diff = cat(1, 1, diff(amplitdue_index));
+    disp('finding threshold crossings')
+    [~, amplitdue_index] = find(IE.data>=Amplitudecutoff);
+    event_diff = cat(2, 1, diff(amplitdue_index));
     transition_points = event_diff>10;
     event_boundaries_index = amplitdue_index(transition_points==1);
     % event_boundaries = IE.T(event_boundaries_index);
     
-    disp('finding peak of candidate events')
     %% By event INDICES
     % demarcate the gamma events in a matrix.
-    lasttouse = length(IE.D)-TimeStepsToChopOFF;
+    lasttouse = length(IE.data)-TimeStepsToChopOFF;
     event_boundaries_touse = event_boundaries_index<lasttouse;
     event_boundaries_index = event_boundaries_index(event_boundaries_touse); % to remove cases where gamma event occurs in last 100ms of the recording
     for iEvent = 1:length(event_boundaries_index);
         t0 = event_boundaries_index(iEvent);
-        datatouse = IE.D(t0: t0 + 100); % look in the next 100ms.   * this is approximate. 100 samples at ~ 1 sample/millisecond
+        datatouse = IE.data(t0: t0 + 100); % look in the next 100ms.   * this is approximate. 100 samples at ~ 1 sample/millisecond
         [~, c] = max(datatouse);
         amplitude_peaks_index(iEvent) = t0 + c;
-        amplitude_peaks_times(iEvent) = IE.T(t0 + c);
+        amplitude_peaks_times(iEvent) = IE.tvec(t0 + c);
     end
     amplitude_peaks_index = amplitude_peaks_index';
     gammaIndicesStart = amplitude_peaks_index - numBoutSamples; gammaIndicesEnd = amplitude_peaks_index + numBoutSamples;
@@ -84,12 +94,12 @@ for iSess = startSess:endSess;
     firstGammaToUse = find(firstTimeStamps>0, 1, 'first');
     for iL = firstGammaToUse:length(amplitude_peaks_index);   % skip event 1 in case the gamma event 'starts' before the first timestamp.
         %     disp(iL);
-        x = ofc.D(amplitude_peaks_index(iL)-(numBoutSamples): amplitude_peaks_index(iL)+(numBoutSamples));
+        x = ofc.data(amplitude_peaks_index(iL)-(numBoutSamples): amplitude_peaks_index(iL)+(numBoutSamples));
         x = x(1:nobs);
-        y = vstr.D(amplitude_peaks_index(iL)-(numBoutSamples): amplitude_peaks_index(iL)+(numBoutSamples));
+        y = vstr.data(amplitude_peaks_index(iL)-(numBoutSamples): amplitude_peaks_index(iL)+(numBoutSamples));
         y = y(1:nobs);
-        assert(size(x,1)==nobs);
-        assert(size(y,1)==nobs);
+        assert(size(x,2)==nobs);
+        assert(size(y,2)==nobs);
         
         X(1,:,iL) = x;
         X(2,:,iL) = y;
@@ -97,18 +107,22 @@ for iSess = startSess:endSess;
     toc
     
     %% plot it
-    if doPlot == 1;
+    if cfg_out.doPlot == 1;
         clf
         hold on
-        EvalKeys;
-        plot(CSC0.range, CSC0.data);
-        plot(IE.range, IE.data, 'r')
+        LoadExpKeys;
+        plot(f_vstr.tvec, f_vstr.data);   % plot the filtered vStr LFP.
+        plot(IE.tvec, IE.data, 'r')      % plot the amplitude envolope 
         line([ExpKeys.TimeOnTrack ExpKeys.TimeOffTrack], [Amplitudecutoff Amplitudecutoff], 'linestyle', '--', 'Color', 'k', 'LineWidth', 1);
-        plot(vstr.T(amplitude_peaks_index), ones(1,length(amplitude_peaks_index)), 'k.', 'MarkerSize', MarkerSize)
-        plot(amplitude_peaks_times, repmat(Amplitudecutoff,1,length(amplitude_peaks_times)), 'g.', 'MarkerSize', MarkerSize)
+        plot(vstr.tvec(amplitude_peaks_index), ones(1,length(amplitude_peaks_index)), 'k.', 'MarkerSize', cfg_out.MarkerSize)
+        plot(amplitude_peaks_times, repmat(Amplitudecutoff,1,length(amplitude_peaks_times)), 'g.', 'MarkerSize', cfg_out.MarkerSize)
+        xlabel('Time (sec)', 'FontSize', 16)
+        ylabel('Voltage (microvolts)', 'FontSize', 16)
+        set(gca, 'FontSize', 16)
+        
     end
     %% save it
-    if SaveIt == 1;
+    if cfg_out.SaveIt == 1;
         if lowpass == 45; gammatype = 'low'; end
         if lowpass == 70; gammatype = 'high'; end
         SSN = GetSSN('SingleSession');
@@ -116,9 +130,7 @@ for iSess = startSess:endSess;
         save(fn, 'gammaIndicesStart', 'gammaIndicesEnd', 'gammaTimesStart', 'gammaTimesEnd', 'X', 'params');
         disp(fn);
         disp('data saved');
-    end
-    
-    %     clear gammaIndicesStart; clear gammaIndicesEnd; clear gammaTimesStart; clear gammaTimesEnd; clear params; clear amplitude_peaks_index; clear amplitude_peaks_times;
+    end    
     popdir;
 end
 
